4b1660763af11321eed69862b0f4d48e76ac6f4f
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2023-04-17 19:54:08 -0500

    added makitFixit for standing up a php-fpm stack

---
+++ makitFixit/fixit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# bash makit.sh "newDns"
+
+echo "In sed"
+oldDNS = (head -n 1 ./configs/creds.txt)
+sed -i "s/$oldDNS/${1}/g" /etc/nginx/sites-available/default
+sed -i "s/$oldDNS/${1}/g" ./configs/cert_ext.cnf
+
+echo "In ssl Generation"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -config configs/cert_ext.cnf
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+echo "nginx fixin"
+service nginx reload
+
+echo ""
+echo "index: https://$oldDNS/"
+"${1}" > ./configs/creds.txt
+echo "dbadmin: https://$oldDNS/dbadmin"
+echo ""
